e556cd6690ba44ab880cf9bafe195dd965a02883
Nawal Nasir <nawal.nasir00@gmail.com>
2021-01-05 20:05:00 -0500

    Update countOfAllBooleansAndStrings.js

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,11 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+  var count = 0;
+  for(let i = 0; i< arr.length; i++){
+    if(typeof arr[i] === 'boolean' || typeof arr[i] === 'string') 
+      count++;
+  }
+
+  return count;
 }
 
 // Do not edit this line;
