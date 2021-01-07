de34f63bcd62e97b86633616ea50d29370b6c029
Nawal Nasir <nawal.nasir00@gmail.com>
2021-01-06 20:41:27 -0500

    Update sumOfAllOddNumbers.js

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,5 +1,9 @@
 function sumOfAllOddNumbers(nums) {
-  // Insert code here;
+ var count = 0;
+  for(let i = 0; i < nums.length; i++){
+    if(nums[i] % 2 === 1) count++;
+  }
+  return count;
 }
 
 // Do not edit this line;
