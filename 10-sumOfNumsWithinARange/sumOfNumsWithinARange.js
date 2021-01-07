e7a028d8aba158132e989a3f0d6f5488f8990ff9
Nawal Nasir <nawal.nasir00@gmail.com>
2021-01-06 20:51:25 -0500

    Update sumOfNumsWithinARange.js

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,10 @@
 function sumOfNumsWithinARange(nums, start, end) {
-  // Insert code here;
+   var counter = 0;
+   for (let i = 0; i < nums.length; i++) {
+      if (nums[i] >= start && nums[i] <= end) 
+        counter++;
+  }
+  return counter;
 }
 
 // Do not edit this line;
