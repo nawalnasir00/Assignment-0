cb0c2dfb5ac1d23f7040ef6eb7437a9364736707
Nawal Nasir <nawal.nasir00@gmail.com>
2021-01-24 16:23:32 -0500

    Update sumOfMinimumAndMaximum.js

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,18 @@
-function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+function sumOfMinimumAndMaximum(array) {
+    var sum = array[0],
+        min = array[0],
+        max = array[0];
+        
+    for (let i = 1; i < array.length; i++) {
+        sum += array[i];
+        if (min > array[i]) min = array[i];
+        if (max < array[i]) max = array[i];
+    }
+
+    return [min + max];
+}
+
+console.log(sumOfMinimumAndMaximum([2,4,5,6,23]));
 }
 
 // Do not edit this line;
