6fda0bcf3bc813847e37d6746e5742c544384976
Nawal Nasir <nawal.nasir00@gmail.com>
2021-01-24 18:00:39 -0500

    Update isPalindrome.js

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,13 @@
-function isPalindrome(word) {
-  // Insert code here;
+function isPalindrome(str) {
+ var re = /[^A-Za-z0-9]/g;
+ str = str.toLowerCase().replace(re, '');
+ var len = str.length;
+ for (var i = 0; i < len / 2; i++) {
+   if (str[i] !== str[len - 1 - i]) {
+       return false;
+   }
+ }
+ return true;
 }
 
 // Do not edit this line;
